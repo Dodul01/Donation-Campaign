8076bd20621a70bc43f9c995617b4fd16dbb9b14
Dodul01 <mhdodul299@gmail.com>
2023-09-26 00:03:32 +0600

    Search functionality added

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,21 +4,28 @@ import DonationCard from "../../Components/DonationCard/DonationCard";
 
 const Donation = () => {
   const [data, setData] = useState([]);
+  const [num, setNum] = useState(4);
+  const [isBtnVisible, setIsBtnVisible] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     const localStorageData = getDataFromLocalStorage();
     setData(localStorageData);
-  },[]);
+  }, []);
+
+  const handleShowAll = () => {
+    setNum(data.length);
+    setIsBtnVisible(false)
+  }
 
   return (
     <div>
       <h1 className="text-2xl font-bold mt-2">Donations: {data.length}</h1>
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-        {data.map((data)=> <DonationCard key={data.id} data={data}></DonationCard>)}
+        {data.slice(0, num).map((data) => <DonationCard key={data.id} data={data}></DonationCard>)}
       </div>
-      {data.length > 4 && 
+      {data.length > 4 && isBtnVisible &&
         <div className="w-[100px] mx-auto">
-          <button className=" bg-[#009444] text-white p-2 rounded-lg my-3 font-semibold">See All</button>
+          <button onClick={handleShowAll} className=" bg-[#009444] text-white p-2 rounded-lg my-3 font-semibold">See All</button>
         </div>
       }
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,46 @@
 import { useEffect, useState } from "react"
-import Banner from "../../Components/Banner/Banner"
 import Card from "../../Components/Card/Card";
 
 const Home = () => {
   const [donations, setDonations] = useState([]);
-  useEffect(()=>{
+  const [displayData, setDisplayData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSearch = () => {
+    const filterValue = donations.filter(item=> item.category.toLowerCase().includes(inputValue.toLowerCase()));
+    setDisplayData(filterValue);
+  }
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  useEffect(() => {
     fetch('data.json')
-    .then((response)=> response.json())
-    .then((data)=> setDonations(data));
-  },[])
+      .then((response) => response.json())
+      .then((data) => {
+        setDonations(data);
+        setDisplayData(data);
+      });
+  }, [])
+
   return (
     <div>
-      <Banner></Banner>
+      <header>
+        <div className="hero min-h-[65vh]" style={{ backgroundImage: 'url(https://i.ibb.co/HFcs0LJ/banner-Image.jpg)' }}>
+          <div className="hero-overlay bg-white bg-opacity-80"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-3xl text-black">
+              <h1 className="text-3xl font-bold mb-3">I Grow By Helping People In Need</h1>
+              <input onChange={handleChange} className="p-[7px] rounded-l-lg lg:w-[300px] md:w-[250px] border border-solid border-gray-400 outline-none" type="text" placeholder="Search Here" />
+              <button onClick={handleSearch} className="bg-[#FF444A] p-2 rounded-r-lg text-white">Search</button>
+            </div>
+          </div>
+        </div>
+      </header>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 my-7">
-        {donations.map((donation)=> <Card key={donation.id} donation={donation}></Card>)}
+        {displayData.map((donation) => <Card key={donation.id} donation={donation}></Card>)}
       </div>
     </div>
   )
